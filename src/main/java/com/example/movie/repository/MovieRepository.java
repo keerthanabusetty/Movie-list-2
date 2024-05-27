1f8d0d905108f208e928784defb0cbaecff201d2
keerthanabusetty <busetty14keerthana@gmail.com>
2024-05-27 06:53:29 +0000

    Movies-List-2

---
+++ src/main/java/com/example/movie/repository/MovieRepository.java
@@ -0,0 +1,17 @@
+// Write your code herepackage com.example.movie;
+package com.example.movie.repository;
+
+import com.example.movie.model.Movie;
+import java.util.*;
+
+public interface MovieRepository {
+    ArrayList<Movie> getMovies();
+
+    Movie getMovieById(int movieId);
+
+    Movie addMovie(Movie movie);
+
+    Movie updateMovie(int movieId, Movie movie);
+
+    void deleteMovie(int movieId);
+}
